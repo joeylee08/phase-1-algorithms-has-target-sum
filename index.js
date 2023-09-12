61cf319ff62e653e67beec3ece40a6ae01f1fd37
Joseph <josephylee72@gmail.com>
2023-09-12 10:42:45 -0700

    completed algorithms has target sum

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if (i === j) continue;
+//       if (array[i] + array[j] === target) return true;
+//     }
+//   }
+//   return false;
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let i = 0;
+  let j = array.length - 1;
+  array.sort((a, b) => a - b);
+  while (i < j) {
+    if (array[i] + array[j] === target) return true;
+    array[i] + array[j] > target ? j-- : i++;
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Option 1: O(n ^ 2) => nested loop
+  Option 2: O(n) => two non-nested iterators
 */
 
 /* 
-  Add your pseudocode here
+  Option 1: Use nested loops to check every pair;
+  Option 2: Sort array from smallest to least, and use i and j variables to check pairs,
+            incrementing and decrementing i or j based on sum > target
+  
 */
 
 /*
